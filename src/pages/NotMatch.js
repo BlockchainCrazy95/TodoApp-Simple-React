b05f5fcc4d70853d74928166ba3ac78aaa49a5af
happyworld0905 <happyworld0905@gmail.com>
2021-11-01 06:25:23 +0300

    first commit

---
+++ src/pages/NotMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotMatch = () => {
+    return (
+        <div>
+            <h3> Error : Page not found</h3>
+        </div>
+    )
+}
+
+export default NotMatch;
